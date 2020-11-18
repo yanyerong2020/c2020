0644e92c428f808ea05233622daa574dc04e3454
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-11-18 09:20:38 +0800

    Create 111802.第一次题目.c

---
+++ 111802.第一次题目.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+int a=3,b=4,c=5;
+int x,y;
+if(!(x=a)&&(y=b)&&0){
+	printf("为真语句\n");
+printf("为假语句\n");
+}
+else if(!(a+b)+c-1&&b+c/2){
+	printf("为真语句\n");
+printf("为假语句\n");
+}
+
+}
