465ae37fedd1836ea674911c1fdbfadd9c98e0da
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-11-25 17:36:36 +0800

    Create 112504.时钟数字循环.c

---
+++ 112504.时钟数字循环.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+	Sleep(1000);
+printf("现在是1秒\n");
+	while(100){
+Sleep(1000);
+printf("2\n");
+Sleep(1000);
+printf("3\n");
+Sleep(1000);
+printf("4\n");
+Sleep(1000);
+printf("5\n");
+Sleep(1000);
+printf("6\n");
+Sleep(1000);
+printf("7\n");
+Sleep(1000);
+printf("8\n");
+Sleep(1000);
+printf("9\n");
+	}
+
+}
