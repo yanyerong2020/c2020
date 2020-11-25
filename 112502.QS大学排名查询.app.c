e9cc6b7ef8866627847654115d458221bbc58976
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-11-25 09:59:35 +0800

    Create 112502.QS大学排名查询.app.c

---
+++ 112502.QS大学排名查询.app.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main(){
+int x;
+printf("QS大学排名查询\n");
+scanf("%d",&x);
+if(x==1){
+printf("哈佛大学\n");
+}
+else if(x==2){
+printf("麻省理工学院\n");
+}
+else if(x==3){
+printf("斯坦福大学\n");
+}
+else if(x==4){
+printf("加州大学伯克利分校\n");
+}
+else if(x==5){
+printf("牛津大学\n");
+}
+else if(x==6){
+printf("加州理工大学\n");
+}
+else if(x==7){
+printf("剑桥大学\n");
+}
+else if(x==8){
+printf("哥伦比亚大学\n");
+}
+else if(x==9){
+printf("普林斯顿大学	\n");
+}
+else if(x==10){
+printf("约翰霍普金斯大学\n");
+}
+else {
+printf("老铁的名次太靠后了，请输入1-10");
+}
+}
