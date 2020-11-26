cdfb3f8a4c7f6c78096fe244e023801f14782f5e
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-11-26 20:38:21 +0800

    Create 112603.降序.app.c

---
+++ 112603.降序.app.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+int x,y,z,t;
+scanf("%d,%d,%d",&x,&y,&z);
+if(x<y)
+{
+t=x;x=y;y=t;
+}
+if(x<z)
+{
+t=x;x=z;z=t;
+}
+
+if(y<z)
+{
+t=y;y=z;z=t;
+}
+printf("%d,%d,%d",x,y,z);
+}
