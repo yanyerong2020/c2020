f08bc1e1a88971686d5d6242dfb39e578f26a98f
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-12-02 17:30:51 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -1,4 +1,4 @@
-##include<stdio.h>
+#include<stdio.h>
 void main(){
 int a,b,c,d;
 printf("请输入秒数\n");
@@ -6,7 +6,11 @@ scanf("%d",&a);
 b=a/3600;
 c=(a%3600)/60;
 d=a-3600*b-60*c;
+if(a>0&&a<86399){
 printf("%02d:%02d:%02d\n",b,c,d);
-
+}
+else {
+printf("你输入的数值超出范围");
+}
 
 }
