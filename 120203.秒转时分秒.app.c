ba1952179d0f210813261e897afc05939f717001
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-12-02 16:55:22 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 void main(){
-int a,b,c;
+int a,b,c,d;
 printf("请输入秒数\n");
 scanf("%d",&a);
 b=a/3600;
 c=(a%3600)/60;
-printf("%d\n",b);
-printf("%d\n",c);
-printf("%d\n",a);
+d=a-3600*b-60*c;
+printf("%d时,%d分,%d秒\n",b,c,d);
 }
