035ecf79c828495169ffb6f9ce5d70c9415ffabf
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-12-02 09:13:40 +0800

    Update 1202.switch语句.app.c

--- 1202.switch语句.app.c
+++ 1202.switch语句.app.c
@@ -4,27 +4,27 @@ int x;
 printf("欢迎使用QS大学排名查询程序\n");
 scanf("%d",&x);
 switch(x){
-		case 1:printf(" 麻省理工学院");break;
-		case 2:printf("	斯坦福大学");break;
-		case 3:printf(" 哈佛大学");break;	
-		case 4:printf("	加州理工学院");break;
-		case 5:printf("	牛津大学");break;
-		case 6:printf("	苏黎世联邦理工学院");break;
-		case 7:printf("	剑桥大学");break;
-		case 8:printf("	帝国理工学院");break;
-		case 9:printf("	芝加哥大学");break;
-		case 10:printf("伦敦大学学院");break;
-		case 11:printf("新加坡国立大学");break;
-		case 12:printf("普林斯顿大学");break;
-		case 13:printf("南洋理工大学");break;
-		case 14:printf("洛桑联邦理工学院");break;
-		case 15:printf("清华大学");break;
-		case 16:printf("宾夕法尼亚大学");break;
-		case 17:printf("耶鲁大学");break;
-		case 18:printf("康奈尔大学");break;
-		case 19:printf("哥伦比亚大学");break;
-		case 20:printf("爱丁堡大学");break;
-		default: printf("输入错误，请输入1-20");
+		case 1:printf(" 麻省理工学院\n");break;
+		case 2:printf("	斯坦福大学\n");break;
+		case 3:printf(" 哈佛大学\n");break;	
+		case 4:printf("	加州理工学院\n");break;
+		case 5:printf("	牛津大学\n");break;
+		case 6:printf("	苏黎世联邦理工学院\n");break;
+		case 7:printf("	剑桥大学\n");break;
+		case 8:printf("	帝国理工学院\n");break;
+		case 9:printf("	芝加哥大学\n");break;
+		case 10:printf("伦敦大学学院\n");break;
+		case 11:printf("新加坡国立大学\n");break;
+		case 12:printf("普林斯顿大学\n");break;
+		case 13:printf("南洋理工大学\n");break;
+		case 14:printf("洛桑联邦理工学院\n");break;
+		case 15:printf("清华大学\n");break;
+		case 16:printf("宾夕法尼亚大学\n");break;
+		case 17:printf("耶鲁大学\n");break;
+		case 18:printf("康奈尔大学\n");break;
+		case 19:printf("哥伦比亚大学\n");break;
+		case 20:printf("爱丁堡大学\n");break;
+		default: printf("输入错误，请输入1-20\n");
 
 }
 
