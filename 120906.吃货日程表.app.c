b8f751cc306d94183750d85c946fee51cf683b36
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-12-09 17:57:00 +0800

    Create 120906.吃货日程表.app.c

---
+++ 120906.吃货日程表.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+	char x1[]="春节";
+	char x2[]="端午";
+	char x3[]="中秋节";
+	char x4[]="元宵";
+	scanf("%s",&x1);
+printf("饺子",x1);
+		scanf("%s",&x2);
+printf("粽子",x2);
+	scanf("%s",&x3);
+printf("月饼",x3);
+		scanf("%s",&x4);
+printf("元宵",x4);
+}
