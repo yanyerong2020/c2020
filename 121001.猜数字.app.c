56ae5501d9e8b8618f2ea6b6258a8dcaf7f1419e
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-12-10 21:43:10 +0800

    Create 121001.猜数字.app.c

---
+++ 121001.猜数字.app.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+printf("  请回答，答对才能进入游戏哦　\n");
+while(1)
+{
+		char x[50];
+		char i[]="柒柒";
+int s=7;
+int y;
+		scanf("%s",&x);
+		if(strcmp(x,i)==0)
+		{
+		printf("  答对了，真棒！　  \n");
+		}
+		else 
+		{
+		printf("  错了错了━━(￣ー￣*|||━━重来(^人^)  \n");
+		}
+scanf("%d",&y);
+printf("友情提示：只能输入1—10哦\n");
+
+if(y==7)
+{
+printf("猜对了，真不错^o^\n");
+}
+else if(y>7)
+{
+printf("猜错了，太大了，重猜╯︿╰\n");
+}
+else
+{
+printf("猜错了，太小了，重猜╯︿╰\n");
+}
