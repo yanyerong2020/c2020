d139f143bdaf9374b7ad7292f5a4c04d107e11d2
yanyerong2020 <74356682+yanyerong2020@users.noreply.github.com>
2020-12-16 07:43:28 +0800

    Update 121001.猜数字.app.c

--- 121001.猜数字.app.c
+++ 121001.猜数字.app.c
@@ -12,24 +12,24 @@ int y;
 		scanf("%s",&x);
 		if(strcmp(x,i)==0)
 		{
-		printf("  答对了，真棒！　  \n");
+		printf("  答对了，真棒! \n");
 		}
 		else 
 		{
-		printf("  错了错了━━(￣ー￣*|||━━重来(^人^)  \n");
+		printf("  错了错了,重来\n");
 		}
 scanf("%d",&y);
 printf("友情提示：只能输入1—10哦\n");
 
 if(y==7)
 {
-printf("猜对了，真不错^o^\n");
+printf("猜对了，真不错\n");
 }
 else if(y>7)
 {
-printf("猜错了，太大了，重猜╯︿╰\n");
+printf("猜错了，太大了，重猜\n");
 }
 else
 {
-printf("猜错了，太小了，重猜╯︿╰\n");
+printf("猜错了，太小了，重猜\n");
 }
